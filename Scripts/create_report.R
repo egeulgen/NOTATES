e953326c47b6e97ef5598c5b78aa337917eed672
Ege Ulgen <egeulgen@gmail.com>
2019-11-02 17:02:35 +0300

    Update create_report.R

--- Scripts/create_report.R
+++ Scripts/create_report.R
@@ -1,30 +1,42 @@
+#!/usr/bin/env Rscript --vanilla
+
 ##################################################
 ## Project: NOTATES
 ## Script purpose: Wrapper to create PDF report
-## Date: Oct 26, 2019
+## Date: Nov 2, 2019
 ## Author: Ege Ulgen
 ##################################################
 
 # Packages and arg.s ------------------------------------------------------
-if(!suppressMessages(require("rmarkdown")))
+if(!suppressPackageStartupMessages(require(rmarkdown)))
   install.packages("rmarkdown")
-if(!suppressPackageStartupMessages(require("markdown")))
-  install.packages("markdown")
 if(!suppressPackageStartupMessages(require(knitr)))
   install.packages("knitr")
-if(!suppressPackageStartupMessages(require("ggplot2")))
+if(!suppressPackageStartupMessages(require(kableExtra)))
+  install.packages("kableExtra")
+if(!suppressPackageStartupMessages(require(ggplot2)))
   install.packages("ggplot2")
 
 suppressPackageStartupMessages(library(rmarkdown))
-args <- commandArgs(trailingOnly=TRUE)
+options(stringsAsFactors = FALSE)
+args <- commandArgs(trailingOnly = TRUE)
 
 # Knit PDF report ---------------------------------------------------------
 ## Copy the template RMD file
-path2reportRMD <- file.path(paste0("Report_", args[1], ".Rmd"))
+reportRMD_fname <- paste0("Report_", args[1], ".Rmd")
 file.copy(file.path(args[2], "Report.Rmd"),
-          path2reportRMD)
+          reportRMD_fname, 
+          overwrite = TRUE)
 
-render(path2reportRMD, "pdf_document", params = list(ID=args[1], script_dir=args[2], exome_length=args[3], type=args[4]))
+## Render report
+render(input = reportRMD_fname, 
+       params = list(ID = args[1], 
+                     script_dir = path.expand(args[2]), 
+                     exome_length = as.numeric(args[3]), 
+                     type = args[4],
+                     primary_tm = as.logical(args[5]),
+                     tumor_sample = args[6]),
+       output_format = "pdf_document")
 
-# cleanup
-unlink(path2reportRMD)
+# Clean up
+unlink(reportRMD_fname)
