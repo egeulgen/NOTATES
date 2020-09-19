f2edff8c6d8a791ed57fc0bb9a9d4bd6d91769e2
Ege Ulgen <egeulgen@gmail.com>
2020-09-19 13:34:56 +0300

    Update ExomeCNV.sh

--- Scripts/ExomeCNV/ExomeCNV.sh
+++ Scripts/ExomeCNV/ExomeCNV.sh
@@ -66,8 +66,13 @@ $GATK HaplotypeCaller \
 $GATK VariantFiltration \
 	-R $genome \
 	-V ./ExomeCNV/baf/raw_tumor_HC.vcf.gz \
-	--filter-expression "QD < 2.0 || MQ < 40.0 || FS > 60.0 || SOR > 4.0 || MQRankSum < -12.5 || ReadPosRankSum < -8.0" \
-	--filter-name "HQ_SNP_filter" \
+	-filter "QD < 2.0" --filter-name "QD2" \
+	-filter "QUAL < 30.0" --filter-name "QUAL30" \
+	-filter "SOR > 3.0" --filter-name "SOR3" \
+	-filter "FS > 60.0" --filter-name "FS60" \
+	-filter "MQ < 40.0" --filter-name "MQ40" \
+	-filter "MQRankSum < -12.5" --filter-name "MQRankSum-12.5" \
+	-filter "ReadPosRankSum < -8.0" --filter-name "ReadPosRankSum-8" \
 	-O ./ExomeCNV/baf/filtered_tumor_HC.vcf.gz
 
 rm ./ExomeCNV/baf/raw_tumor_HC.vcf.gz*
