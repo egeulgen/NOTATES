254a3d93cfe6f0fe31be0d370ff82dc152d085d8
Ege Ulgen <egeulgen@gmail.com>
2020-09-24 12:06:06 +0300

    Update fastqc.sh

--- Scripts/fastqc.sh
+++ Scripts/fastqc.sh
@@ -10,7 +10,7 @@
 name=$1
 
 echo "################################## Running FASTQC for ""$name    " $(date)
-cd ./"$name"
+cd "$name"
 mkdir QC
 
 lanes=$(cat ./lanes.txt)
@@ -18,6 +18,6 @@ lanes=$(cat ./lanes.txt)
 for lane in $lanes
 do
 	echo "$lane"
-	fastqc --nogroup "$lane"_R1.fastq.gz "$lane"_R2.fastq.gz --outdir=./QC
+	fastqc --nogroup "$lane"_R1.fastq.gz "$lane"_R2.fastq.gz --outdir=QC
 done
 cd ..
