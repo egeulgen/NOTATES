a8ae1aa38af7728d4be8b58e5f5ccbfa1249c6d8
Ege Ulgen <egeulgen@gmail.com>
2020-09-26 19:54:29 +0300

    fix signatures

--- Scripts/DeConstructSigs.R
+++ Scripts/DeConstructSigs.R
@@ -22,12 +22,8 @@ sample_id <- arguments[2]
 # load somatic SNVs -------------------------------------------------------
 somatic_SNVs <- read.delim("Funcotator/annotated_somatic.maf", comment.char="#")
 
-# Subsetting for (MuTect's default) HQ filters
-somatic_SNVs <- subset(somatic_SNVs, 
-                       alt_allele_seen=="True")
 # filter for VAF > 0.05
 somatic_SNVs <- somatic_SNVs[somatic_SNVs$tumor_f > 0.05, ]
-
 
 somatic_SNVs$Sample <- sample_id
 somatic_SNVs <- somatic_SNVs[,c("Sample","Chromosome", "Start_position","Reference_Allele", "Tumor_Seq_Allele2")]
