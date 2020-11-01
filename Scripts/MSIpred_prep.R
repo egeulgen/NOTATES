043156613408becc64f88657da4c46114b46a428
Ege Ulgen <egeulgen@gmail.com>
2020-11-01 13:37:46 +0300

    filter for primary contigs

--- Scripts/MSIpred_prep.R
+++ Scripts/MSIpred_prep.R
@@ -15,4 +15,10 @@ funcotator_maf$Tumor_Sample_Barcode <- args[1]
 nec_cols <- c("Chromosome", "Start_Position", "End_Position", "Variant_Classification", "Variant_Type", "Tumor_Sample_Barcode")
 funcotator_maf <- funcotator_maf[, nec_cols]
 
+chr.list <- c("chr1", "chr2", "chr3", "chr4", "chr5", "chr6", 
+              "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", 
+              "chr13", "chr14", "chr15", "chr16", "chr17", "chr18",
+              "chr19", "chr20", "chr21", "chr22", "chrX", "chrY")
+funcotator_maf <- funcotator_maf[funcotator_maf$Chromosome %in% chr.list, ]
+
 write.table(funcotator_maf, "MSIpred/somatic_maf.maf", sep = "\t", quote = FALSE, row.names = FALSE) 
